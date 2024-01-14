c12293cc07a153f8e1f65b3613f7be1dcd27a71a
mikeadonnelly <mdonn599@gmail.com>
2024-01-14 02:54:41 -0500

    changes made from week 4 lab

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+import RemainingBudget from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -17,18 +21,33 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                                            // Budget component
+                            <div className='col-sm'>
+                            <Budget />
+                            </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                            //Remaining component
+                            <div className='col-sm'>
+                            <Remaining />
+                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                         //ExpenseTotal component
+                            <div className='col-sm'>
+                            <ExpenseTotal />
+                            </div>
                         }        
                        
                         {
                             /* Add ExpenseList component here */
+                            <div className='col-sm'>
+                            <ExpenseList />
+                            </div>
                         }         
 
                         {
@@ -37,6 +56,9 @@ const App = () => {
 
                         {
                             /* Add AllocationForm component here under */
+                            <div className='col-sm'>
+                            <AllocationForm/>
+                            </div>
                         }        
 
                 </div>
